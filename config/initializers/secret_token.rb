0f853baad0d5d01adde3cb038e3099f1f6a43c39
tanihito <tanihito@livedoor.com>
2012-10-13 15:36:38 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoffeeExample::Application.config.secret_token = 'eea7a8184b2249af074818f09acb112113e2453fcc3124892dc692e4289c4eccaf87dd7cc0bf8298d9ccb53fe4f0e34761e0dc5d81900dd46ae2c76b8070a9b9'
